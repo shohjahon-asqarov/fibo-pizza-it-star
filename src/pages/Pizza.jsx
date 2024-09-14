a4b985969f1ddea70bb0c82e8db331c2dafcf42e
shoxjahon_asqarov <shohjahonasqarov.web@gmail.com>
2024-09-14 13:55:23 +0500

    page title change

--- src/pages/Pizza.jsx
+++ src/pages/Pizza.jsx
@@ -3,11 +3,10 @@ import ProductCard from '../components/ProductCard';
 
 const Pizza = () => {
 
-
     return (
         <section>
             <div className="container">
-                <h2>Паста</h2>
+                <h2>Пицца</h2>
 
                 <ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-10 pb-20'>
                     {pizza_data.map(pizza => {
