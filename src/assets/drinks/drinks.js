31e6160306ba47d6b2d8b3b913e888333a091f23
kenzoslaby <kenzoslaby@gmail.com>
2024-09-18 17:35:48 +0500

    paste drinks add language

--- src/assets/drinks/drinks.js
+++ src/assets/drinks/drinks.js
@@ -9,58 +9,130 @@ import s6 from './suv6.png';
 const drinks = [
     {
         id: 40,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s1,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s1",
         price: 120,
         count: 1,
-        category: 'Напитки'
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
     },
     {
         id: 41,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s2,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s2",
         price: 120,
         count: 1,
-        category: 'Напитки'
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
     },
     {
         id: 42,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s3,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s3",
         price: 120,
         count: 1,
-        category: 'Напитки'
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
     },
     {
         id: 43,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s4,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s4",
         price: 120,
         count: 1,
-        category: 'Напитки'
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
     },
     {
         id: 44,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s5,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s5",
         price: 120,
         count: 1,
-        category: 'Напитки'
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
     },
     {
         id: 45,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
-        img: s6,
+        title: {
+            ru: "Дарим кибер-призы",
+            en: "We are giving away cyber-prizes",
+            uz: "Kiber-sovg'alarni hadya qilamiz"
+        },
+        description: {
+            ru: "Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.",
+            en: "What an achievement! Order a Cyber Combo and get access to games from MY.GAMES, along with a coconut bar and 'Cyber' chocolate cookie from Bite. Also, automatically become a participant in the giveaway of game keys and large pizzas on June 29.",
+            uz: "Mana qanday muvaffaqiyat! Kiber-kombo buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini oling, shuningdek, Bite kompaniyasining kokos batonchasi va «Cyber» shokoladli pechenesini qo‘lga kiriting. Shuningdek, 29-iyun kuni o‘yin kalitlari va katta pizzalar uchun avtomatik ravishda o‘yinda ishtirok etasiz."
+        },
+        img: "s6",
         price: 120,
         count: 1,
-        category: 'Напитки'
-    },
+        category: {
+            ru: "Напитки",
+            en: "Drinks",
+            uz: "Ichimliklar"
+        }
+    }
 ];
 
 export { drinks }
--- src/assets/paste/pasta.js
+++ src/assets/paste/pasta.js
@@ -10,76 +10,172 @@ import p8 from './pasta8.jpeg';
 const paste = [
     {
         id: 9,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p1,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 10,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p2,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 11,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p3,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 12,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p4,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 13,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p5,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 14,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p6,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 15,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p7,
         count: 1,
-        category: 'Паста'
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
     },
     {
         id: 16,
-        title: "С креветками и трюфелями",
-        description: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан.350 г",
+        title: {
+            ru: "С креветками и трюфелями",
+            en: "With Shrimps and Truffles",
+            uz: "Krevetka va trufellar bilan"
+        },
+        description: {
+            ru: "Домашнаяя паста феттуччине, сливочный соус, креветки, трюфельное масло, черный перец, пармезан. 350 г",
+            en: "Homemade fettuccine pasta, creamy sauce, shrimps, truffle oil, black pepper, parmesan. 350g",
+            uz: "Uy qurilishi fettuccine pastasi, qaymoqli sous, krevetkalar, trufel yog'i, qora murch, parmezan. 350 g"
+        },
         price: 470,
         img: p8,
         count: 1,
-        category: 'Паста'
-    },
+        category: {
+            ru: "Паста",
+            en: "Pasta",
+            uz: "Pasta"
+        }
+    }
 ];
 
 export { paste }
