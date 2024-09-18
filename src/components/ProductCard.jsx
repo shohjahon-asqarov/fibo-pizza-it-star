94c556ec174c0639b9c9667b4deec719b0e74007
azamat-sharifov <sharipovzahriddin71@gmail.com>
2024-09-18 07:20:11 -0700

    add supi language

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,9 +4,13 @@ import { toggleLike } from "../store/slices/likeSlice";
 import { likeFillIcon, likeIcon } from "../assets/data";
 import { addCard } from "../store/slices/cardSlice";
 import { toggleModal } from "../store/slices/modalSlice";
+import { useTranslation } from "react-i18next";
 
 const ProductCard = ({ product }) => {
     const dispatch = useDispatch();
+
+    const {t , i18n} = useTranslation('supi')
+    const currentLanguage = i18n.language;
     return (
         <li data-aos-duration="1000" data-aos="fade-up" className='p-4 relative hover:shadow-lg group cursor-pointer duration-300 border border-[#F3F3F7] rounded-14 flex flex-col' >
             <button onClick={() => dispatch(toggleLike(product))} className='absolute top-5 right-5'>
@@ -14,13 +18,13 @@ const ProductCard = ({ product }) => {
             </button>
             <img onClick={() => dispatch(toggleModal(product))} className='w-64 mx-auto' src={product.img} alt={product.title} />
             <div className='space-y-3 flex-grow'>
-                <h3 >{product.title}</h3>
-                <p className='text-13 font-medium text-cart-description'>{product.description}</p>
+                <h3 >{product.title[currentLanguage]}</h3>
+                <p className='text-13 font-medium text-cart-description'>{product.description[currentLanguage]}</p>
             </div>
             <div className='flex justify-between items-center mt-3'>
                 <p className='text-22 font-bold'>от {product.price} ₽</p>
                 <button
-                    onClick={() => dispatch(addCard(product))}
+                    onClick={() => dispatch(addCard(product))[currentLanguage]}
                     className='btn-yellow !py-1.5 !text-white duration-300 text-sm group-hover:!text-gray   '>
                     В корзину
                 </button>
