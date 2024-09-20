b76a4e96157f98cdff4c5baf9723684fe89705f1
XasanboyKarimov <xkarimov257@gmail.com>
2024-09-20 15:41:31 +0500

    salad stock translate

--- src/assets/salad/salads.js
+++ src/assets/salad/salads.js
@@ -10,77 +10,173 @@ import s8 from './salat8.png';
 const salads = [
     {
         id: 30,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s1,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 31,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s2,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 32,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s3,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 33,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s4,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 34,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s5,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 35,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s6,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 36,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s7,
         price: 230,
         count: 1,
-        category: 'Салаты'
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
     },
     {
         id: 37,
-        title: 'Дарим кибер-призы',
-        description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+        title: {
+            ru: 'Дарим кибер-призы',
+            en: 'We Give Cyber Prizes',
+            uz: 'Biz kiber sovg’alar beramiz'
+        },
+        description: {
+            ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+            en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+            uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+        },
         img: s8,
         price: 230,
         count: 1,
-        category: 'Салаты'
-    },
-
+        category: {
+            ru: 'Салаты',
+            en: 'Salads',
+            uz: 'Salatlar'
+        }
+    }
 ];
+
 
 export { salads }
--- src/assets/stock/stock.js
+++ src/assets/stock/stock.js
@@ -1,77 +1,175 @@
-import stock__img from './stock-img.jpg'
+import stock__img from './stock-img.jpg';
+
 const stock_data = [
   {
     id: 1,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 2,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 3,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 4,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 5,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 6,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 7,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 8,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 9,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 10,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 11,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
   },
   {
     id: 12,
     img: stock__img,
-    title: 'Дарим кибер-призы',
-    description: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.'
-  },
-]
+    title: {
+      ru: 'Дарим кибер-призы',
+      en: 'We Give Cyber Prizes',
+      uz: 'Biz kiber sovg’alar beramiz'
+    },
+    description: {
+      ru: 'Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber» от Bite. А также станьте автоматическим участником розыгрыша игровых ключей и больших пицц 29 июня.',
+      en: 'What an achievement! Order the Cyber Combo and get access to games from MY.GAMES, plus a coconut bar and Cyber chocolate cookie from Bite. Also, automatically become a participant in the draw for game keys and large pizzas on June 29.',
+      uz: 'Qanday muvaffaqiyat! Kiber-komboni buyurtma qiling va MY.GAMES o‘yinlariga kirish huquqini qo‘lga kiriting, shuningdek, Bite’dan kokos batonchasi va “Cyber” shokoladli pechene. 29-iyun kuni o‘yin kalitlari va katta pitsalar yutug‘i uchun avtomatik ishtirokchi bo‘ling.'
+    }
+  }
+];
+
 
 export { stock_data }
