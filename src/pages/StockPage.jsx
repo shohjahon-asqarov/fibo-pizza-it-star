a168ceef6097b0eac40510b6a653a28a52194815
shoxjahon_asqarov <shohjahonasqarov.web@gmail.com>
2024-09-25 20:42:27 +0500

    font size changed h2 and change direction bot folder

--- src/pages/StockPage.jsx
+++ src/pages/StockPage.jsx
@@ -12,11 +12,11 @@ const StockPage = () => {
       <div className="container">
         <h2 data-aos="fade-up">{t('promotions')}</h2>
 
-        <ul className='grid grid-cols-3 gap-5 py-10'>
+        <ul className='grid sm:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
           {stock_data.map((i) => {
             return (
               <li key={i.id} className='pb-5 border border-[#f3f3f7] cursor-pointer   hover:shadow-discount-cart duration-300 group rounded-14 mb-3'>
-                <img className='mb-2' src={i.img} alt={i.title[currentLanguage]} />
+                <img className='mb-2 w-full' src={i.img} alt={i.title[currentLanguage]} />
                 <div className='px-5  space-y-3'>
                   <h3 className='font-bold text-black text-xl'>{i.title[currentLanguage]}</h3>
                   <p className='font-medium text-[#797979] text-13'>{i.description[currentLanguage]}</p>
